9ef36e8076a8f259310a6249fe3b94efea4f5962
ganeshraj-a <79321086+ganeshraj-a@users.noreply.github.com>
2024-03-02 22:36:12 +0530

    Create : case_operation.sql

---
+++ Basic commends/case_operation.sql
@@ -0,0 +1,13 @@
+--Display patient's full name, height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals,
+--birth_date, gender non abbreviated.
+--Convert CM to feet by dividing by 30.48.
+--Convert KG to pounds by multiplying by 2.205.
+
+--1
+select concat(first_name," ",last_name), 
+round(height/30.48,1), round(weight*2.205,0),birth_date,
+case
+	when gender = 'M' then 'MALE'
+	else 'FEMALE'
+	end as gender
+from patients;
